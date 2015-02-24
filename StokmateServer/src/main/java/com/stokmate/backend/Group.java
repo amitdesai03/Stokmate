099afe8a2e8bb186e04fc6011de39dea62ec6838
Amit Desai <amit.desai03@gmail.com>
2015-02-23 17:02:03 -0800

    Working server rest api

---
+++ StokmateServer/src/main/java/com/stokmate/backend/Group.java
@@ -0,0 +1,31 @@
+package com.stokmate.backend;
+
+public class Group extends Response{
+    private long id;
+    private String name;
+    private String admin;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(String admin) {
+        this.admin = admin;
+    }
+}
